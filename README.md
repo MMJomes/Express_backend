bac33817dc734422138c04623bef07eded664af6
unknown <HeinHtet>
2023-03-24 22:41:11 +0630

    completed

---
+++ README.md
@@ -0,0 +1,15 @@
+# Image_upload
+
+## Build Setup
+
+```bash
+# to run express server
+$ npm start
+
+# default port of backend serve is 4000
+$ http://localhost:4000/
+
+# to get all upload image at this api
+# meth GET
+$ http://localhost:4000/api/mydata
+```` 
--- api/mydata.js
+++ api/mydata.js
@@ -6,7 +6,9 @@ app.use('/photo', express.static('uploads'))
 app.use('/', router)
 router.get('/', (req, res) => {
     read_json_from_file = fs.readFileSync('data.json')
+    console.log(read_json_from_file)
     data = JSON.parse(read_json_from_file)
+    console.log(data);
     res.json(data)
 })
 module.exports = router
